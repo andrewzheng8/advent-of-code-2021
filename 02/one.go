8e6a256e2a6d4324ca0b630959fbc1b177d2898e
Andrew Zheng <andrewzheng@hey.com>
2021-12-02 00:56:50 -0500

    finished part one

--- 02/one.go
+++ 02/one.go
@@ -3,14 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	Action = iota
+	Steps
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
+	depth, fwd := 0, 0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		tokens := strings.Split(scanner.Text(), " ")
+
+		steps, _ := strconv.Atoi(tokens[Steps])
+
+		switch tokens[Action] {
+		case "forward":
+			fwd += steps
+		case "down":
+			depth += steps
+		case "up":
+			depth -= steps
+		default:
+			log.Fatalf("Case not handled: %s \n", tokens[Action])
+		}
 	}
+
+	fmt.Printf("At Depth [%d] and Forward [%d] \n", depth, fwd)
+  fmt.Printf("Product is [%d]\n", depth * fwd)
 
 }
